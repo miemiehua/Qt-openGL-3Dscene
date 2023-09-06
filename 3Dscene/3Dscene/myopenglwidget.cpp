e64695bb8f4de5b5e9dee16e00b92e403e0d7bea
王小燕 <wangxy@hzxdata.com>
2023-09-06 11:23:03 +0800

    。。。。。。

--- 3Dscene/3Dscene/myopenglwidget.cpp
+++ 3Dscene/3Dscene/myopenglwidget.cpp
@@ -3,9 +3,38 @@
 myopenglWidget::myopenglWidget(QWidget *parent)
     :QOpenGLWidget(parent)
 {
-
+    initializeGL();
+    resizeGL(100,100);
+    paintGL();
 }
 
 myopenglWidget::~myopenglWidget()
 {
+}
+
+void myopenglWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+    // 设置背景颜色
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void myopenglWidget::resizeGL(int w, int h)
+{
+
+}
+
+void myopenglWidget::paintGL()
+{
+
+}
+
+void myopenglWidget::paintEvent(QPaintEvent *e)
+{
+
+}
+
+void myopenglWidget::resizeEvent(QResizeEvent *e)
+{
+
 }
--- 3Dscene/3Dscene/myopenglwidget.h
+++ 3Dscene/3Dscene/myopenglwidget.h
@@ -3,13 +3,32 @@
 
 #include <QWidget>
 #include <QOpenGLWidget>
+#include <QPaintEvent>
+#include <QResizeEvent>
+#include <QOpenGLFunctions_3_3_Core>
 
-class myopenglWidget : public QOpenGLWidget
+class myopenglWidget : public QOpenGLWidget,protected QOpenGLFunctions_3_3_Core
 {
+    Q_OBJECT
+
 public:
     myopenglWidget(QWidget *parent = nullptr);
     ~myopenglWidget();
+
+protected:
+
+    void initializeGL() override;
+
+    void resizeGL(int w, int h) override;
+
+    void paintGL() override;
+
+    void paintEvent(QPaintEvent *e) override;
+
+    void resizeEvent(QResizeEvent *e) override;
+
 private:
+
 };
 
 #endif // MYOPENGLWIDGET_H
